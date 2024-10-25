239a4d76d402aa130b327a94ccb192f50f29aa9d
antoine serry <antoine.serry@hublex.fr>
2024-10-25 10:06:06 +0200

    toutes les methodes avec la base de données

---
+++ controllers/usersControllers.js
@@ -0,0 +1,108 @@
+const db = require("../database");
+
+exports.getAllUsers = (req, res) => {
+  db.all('SELECT * FROM users', [], (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(rows);
+    }
+  });
+}
+
+exports.getUser = (req, res) => {
+  const { id } = req.params;
+  db.get('SELECT * FROM users WHERE id = ?', [id], (err, row) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else if (row) {
+      res.json(row);
+    } else {
+      res.status(404).json({ message: "Utilisateur non trouvé" });
+    }
+  });
+}
+
+exports.createNewUser = (req, res) => {
+  const { firstName, lastName } = req.body
+  
+  function isAlphanumeric(str) {
+    const regex = /^[a-zA-Z0-9]+$/
+    return regex.test(str)
+  }
+  
+  if (!firstName || !lastName) return res.status(400).json({ error: "Le prénom et le nom de famille sont requis !" })
+  if (typeof firstName !== "string") return res.status(400). json({ error: "That's a weird name !" })
+  if (!isAlphanumeric(firstName)) return res.status(400). json({ error: "That name is not allowed !" })
+  
+  db.run(
+    "INSERT INTO users (firstName, lastName ) VALUES (?, ?)",
+    [firstName, lastName],
+    function (err) {
+      if (err) {
+        res.status(500). json({ error: err.message })
+      } else {
+        res.status(201). json({ id: this.lastID, firstName })
+      }
+    }
+  )
+}
+
+// POST mise à jour d'un utilisateur en fonction de son ID
+exports.updateUser = (req, res) => {
+	const { firstName, lastName } = req.body
+
+	// Récupérer l'id des paramètres
+	const userId = req.params.id
+
+	// Vérifier les champs envoyés
+	let updateFields = []
+	let queryParams = []
+
+	if (firstName) {
+		updateFields.push("firstName = ?")
+		queryParams.push(firstName)
+	}
+
+	if (lastName) {
+		updateFields.push("lastName = ?")
+		queryParams.push(lastName)
+	}
+
+	if (updateFields.length > 0) {
+		// Ajouter userId aux paramètres de la requête
+		queryParams.push(userId)
+
+		// Construire la requête dynamiquement
+		const query = `UPDATE users SET ${updateFields.join(", ")} WHERE id = ?`
+
+		db.run(query, queryParams, function (err) {
+			if (err) {
+				res.status(500).json({ error: err.message })
+			} else if (this.changes === 0) {
+				res.status(404).json({ message: "Utilisateur non trouvé" })
+			} else {
+				res.json({ msg: "Utilisateur mis à jour", userId, firstName, lastName })
+			}
+		})
+	} else {
+		res.status(400).json({ message: "Aucun champ à mettre à jour" })
+	}
+}
+
+exports.deleteUser = (req, res) => {
+	// get the id from the params
+	const { id } = req.params
+	// run the query
+	db.run("DELETE FROM users WHERE id = ?", [id], function (err) {
+		if (err) {
+			res.status(500).json({ error: err.message })
+		} else if (this.changes === 0) {
+			// if nothing found
+			res.status(404).json({ message: "User not found" })
+		} else {
+			// is successful
+			res.status(200).json({ message: "User deleted !" })
+		}
+	})
+}
--- routes/users.js
+++ routes/users.js
@@ -1,62 +1,16 @@
-const express = require("express")
-const router = express.Router()
-const db = require("../database")
+const express = require("express");
+const router = express.Router();
+const { getAllUsers, getUser, createNewUser, updateUser, deleteUser } = require("../controllers/usersControllers");
 
 // GET : LIRE tous les utilisateurs
-router.get("/users/", (req, res) => {
-    db.all('SELECT * FROM users', [], (err, rows) => {
-        if (err) {
-          res.status(500).json({ error: err.message });
-        } else {
-          res.json(rows);
-        }
-    });
-})
+router.get("/users/", getAllUsers);
 
-router.get("/users/:id", (req, res) => {
-    console.log(req.body);
-    const userIndex = users.findIndex((user) => user.id === parseInt(req.params.id));
-    if(userIndex < 0) return res.status(404).json({ msg: "utilisateur non trouvé" });   
-    res.json(users[userIndex]);
-})
+router.get("/users/:id", getUser)
 
-router.put("/users/:id", (req, res)=> {
-    const id = parseInt(req.params.id);
-	const userIndex = users.findIndex((user) => user.id === id);
-    if(userIndex < 0) return res.status(404).json({ msg: "utilisateur non trouvé" });
-    const { firstName, lastName } = req.body
-	if (firstName) users[userIndex].firstName = firstName
-	if (lastName) users[userIndex].lastName = lastName
-    res.json({
-		msg: "utilisateur mis à jour",
-		user: users[userIndex],
-	})
-})
+router.put("/users/:id", updateUser)
 
+router.post("/users/", createNewUser)
 
-router.post("/users/", (req, res) => {
-    console.log(req.body);
-    const { firstName, lastName, role } = req.body;
-    const lastUserId = users[users.length-1].id;
-    const newUser = {
-        id : lastUserId + 1,
-		firstName,
-		lastName,
-		role,
-	}
-    
-    users.push(newUser);
-    res.status(201).json(newUser)
-})
-
-router.delete("/users/:id", (req, res) => {
-    console.log(req.body);
-    const userIndex = users.findIndex((user) => user.id === parseInt(req.params.id));
-    if(userIndex < 0) return res.status(404).json({ msg: "utilisateur non trouvé" });
-    users.splice(userIndex, 1);
-    res.json({
-		msg: "utilisateur supprimé",
-	})
-})
+router.delete("/users/:id", deleteUser)
 
 module.exports = router
